9acad002f87efbb7d3fdf2752ae200a992e9f994
andreapatervilla <andreapatervilla@gmail.com>
2021-05-09 00:44:43 -0500

    Creacion de proyecto

---
+++ src/main/java/Data/ClaseData.java
@@ -0,0 +1,34 @@
+package Data;
+
+public class ClaseData {
+
+
+    public String url ="http://automationpractice.com/index.php";
+    public String strLogin = "corre1234567@gmail.com";
+    public String strEmail = "nuevoff1234567@gmail.com";
+    public String strPassword = "test12345";
+    public String strValidHomePageLogin = "ALREADY REGISTERED?";
+    public String strValidHomePagePersonalInfo = "YOUR PERSONAL INFORMATION";
+    public String strGenero = "F";
+    public String strFirstName = "Andrea";
+    public String strLastName = "Villalobos";
+    public int strDay = 12;
+    public int strMonth = 4;
+    public String strYear = "2003";
+    public String strCompany = "Company de Andrea";
+    public String strAddress = "Playa Blancas";
+    public String strAddressLine = "Cobeñas";
+    public String strCity = "New York";
+    public String strState = "California";
+    public String strPostCode = "00001";
+    public String strCountry = "United States";
+    public String strOther = "Compañia de TI United States";
+    public String strPhone = "31345678";
+    public String strPhoneMobile = "31300000";
+    public String strAlias = "Andrea Villalobos Cobeñas";
+    public String strMyAccount = "MY ACCOUNT";
+
+
+
+
+}
